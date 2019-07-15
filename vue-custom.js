d015a671921a724e113b4863ad24de8cdffa8a13
pg-tech <pg.skybase@gmail.com>
2019-07-15 16:23:02 +0530

    Nested Component

--- vue-custom.js
+++ vue-custom.js
@@ -1,4 +1,37 @@
-Vue.component('task', {
+Vue.component('task-list', {
+    template: `<div>
+                <task-item v-for="(task, id) in tasks" :key="id">
+                    {{ task.name }}
+                </task-item>
+               </div>`,
+    data() {
+        return {
+            tasks: [{
+                id: 1,
+                name: 'work',
+                complete: true
+            },
+            {
+                id: 2,
+                name: 'school',
+                complete: false
+            },
+            {
+                id: 3,
+                name: 'park',
+                complete: false
+            },
+            {
+                id: 4,
+                name: 'India',
+                complete: true
+            }
+            ]
+        }
+    }
+});
+
+Vue.component('task-item', {
     template: `<li>
                 <span>{{action}}</span>
                 <slot></slot>
